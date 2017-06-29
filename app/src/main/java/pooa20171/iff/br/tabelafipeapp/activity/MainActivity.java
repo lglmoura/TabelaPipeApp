0e65f1056eb9fe77a85b8223c362b20dd7629d0f
Luiz Gustavo <lglmoura@gmail.com>
2017-06-29 16:58:56 -0300

    Alterando o Main

---
+++ app/src/main/java/pooa20171/iff/br/tabelafipeapp/activity/MainActivity.java
@@ -0,0 +1,29 @@
+package pooa20171.iff.br.tabelafipeapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import pooa20171.iff.br.tabelafipeapp.R;
+import pooa20171.iff.br.tabelafipeapp.helper.RestFullHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    RestFullHelper http;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(this.getClass().getSimpleName(),"Principal");
+        http = new RestFullHelper();
+
+        //JSONObject json = http.doGet("http://fipeapi.appspot.com/api/1/carros/marcas.json");
+    }
+    public void irParaLivro(View v){
+        Intent marca = new Intent(this,MarcaActivity.class);
+        startActivity(marca);
+    }
+}
